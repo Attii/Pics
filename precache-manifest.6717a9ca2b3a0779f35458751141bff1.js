85cefd4e8b8ff9d1c92398b21b045d1c89b0d4cf
Attii <attij@yandex.ru>
2019-11-21 00:49:32 -0500

    Updates

---
+++ precache-manifest.6717a9ca2b3a0779f35458751141bff1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51b8399ad8bab0f9d7f71c6a0b8aa5b",
+    "url": "/Attii/Pics.git/index.html"
+  },
+  {
+    "revision": "088c9f14d1d7b7549ff8",
+    "url": "/Attii/Pics.git/static/css/main.0f0ce5c6.chunk.css"
+  },
+  {
+    "revision": "3a21110d5d0b0260a01f",
+    "url": "/Attii/Pics.git/static/js/2.95487276.chunk.js"
+  },
+  {
+    "revision": "088c9f14d1d7b7549ff8",
+    "url": "/Attii/Pics.git/static/js/main.f341c3ef.chunk.js"
+  },
+  {
+    "revision": "015c04c0255705f6a7a8",
+    "url": "/Attii/Pics.git/static/js/runtime-main.8b631784.js"
+  }
+]);
